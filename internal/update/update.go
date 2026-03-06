c97c64027e4b91b86f07b2954f9b3eccafbdb233
bluelight <y15826227155@yahoo.com>
2026-03-07 01:09:26 +0800

    update

--- internal/update/update.go
+++ internal/update/update.go
@@ -19,8 +19,8 @@ import (
 )
 
 const (
-	updateUrl    = "https://github.com/bestruirui/octopus/releases/latest/download"
-	updateApiUrl = "https://api.github.com/repos/bestruirui/octopus/releases/latest"
+	updateUrl    = "https://github.com/bluelightgit/octopus/releases/latest/download"
+	updateApiUrl = "https://api.github.com/repos/bluelightgit/octopus/releases/latest"
 )
 
 type LatestInfo struct {
